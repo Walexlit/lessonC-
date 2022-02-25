3cfd85b95cabda9a408a6e25f9ffe485569773e8
Александр Литвинов <sbawalexlit2@gmail.com>
2022-02-25 22:35:33 +0300

    рандом к 003

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA =43;
-int numberB =24;
+﻿int numberA = new Random().Next(1,24);
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,24);
+Console.WriteLine (numberB);
 int result = numberA+numberB;
 Console.WriteLine (result);
