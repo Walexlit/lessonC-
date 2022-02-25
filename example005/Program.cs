a6009813fdd2f627d118afd2118914d563010b5e
Александр Литвинов <sbawalexlit2@gmail.com>
2022-02-25 23:41:24 +0300

    приветствие пса

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "псина")
+{
+     Console.WriteLine("здарова пёс");
+}
+else
+{
+Console.WriteLine("приветствую");
+Console.WriteLine(username);
+}
