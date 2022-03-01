df8646ea270babe7fd526d3c2645109470d47978
Александр Литвинов <sbawalexlit2@gmail.com>
2022-03-01 12:59:19 +0300

    поиск числа по индексу массива

---
+++ example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 25, 3, 4, 45, 88, 78, 88 };
+int n = array.Length;
+int find = 88;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+   
+    }
+    index++;
+}
