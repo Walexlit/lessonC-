30e4d62bc6bc6c616558a06b71ffb0e55a99c8f6
Александр Литвинов <sbawalexlit2@gmail.com>
2022-03-02 21:03:42 +0300

    рандомные числа

---
+++ example011/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int[] Collection)
+{
+    int Length = Collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        Collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
